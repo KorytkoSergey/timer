eb9555f56cdc911c3ca7bffe0a0cd57a3b11946e
KorytkoSergey <korytkosergei28@gmail.com>
2023-03-31 20:04:01 +0600

    Сделано: 1)Написан код для таймера Помодоро. 2)Установлена взаимосвязь между всеми окнами. Нужно: 1)Исправить ошибку со кнопками Стоп и Сброс в Помодоро.(как впрочем и в таймере). 2)Доработать дизайн и добавить звуковое оповещение.

--- Screen_pomodoro.py
+++ Screen_pomodoro.py
@@ -1,25 +1,114 @@
 import tkinter as tk
 import time
 
-def pomodoro_timer(value, lbl_big_time):
-    duration = int(value)
+global lbl_main_time, lbl_second_time, lbl_amount, lbl_info, lbl_big_time, lbl_small_time, flag_stop, flag_drop
+global time_format, root
+
+
+def btm_timer_command():  # вызываем через функцию, чтобы избежать циклов вызовов
+    from Screen_timer import timer_window
+    root.destroy()
+    timer_window()
+
+
+def pomodoro_timer(main_time, second_time, amount):  # собираем, обрабатываем и распределяем
+    global lbl_info, lbl_big_time, lbl_small_time, lbl_amount, flag_drop, flag_stop
+    flag_drop = False
+    flag_stop = False
+    value_lst = [main_time, second_time, amount]
+    value_lst = check_value(value_lst)
+    main_time = value_lst[0]
+    second_time = value_lst[1]
+    amount = value_lst[2]
+    main_duration = float(main_time) * 60  # сохраняем начальное значение времени
+    second_duration = float(second_time) * 60
+    for i in range(int(amount)):
+        if (flag_drop is False) or (flag_stop is False):
+            slices_format = "{:02d}".format(int(int(amount) - i))
+            lbl_amount.configure(text=slices_format)
+            period(main_duration)  # функция основного времени
+            timeout(second_duration)  # функция перерыва
+        if flag_stop or flag_drop:
+            if flag_stop:
+                return stop_to_go()
+            if flag_drop:
+                return total_drop()
+
+
+def period(main_duration):  # функция основного времени
+    global flag_stop, flag_drop, time_format
+    flag_drop = False
+    flag_stop = False
     start_time = time.time()
-
-    while True:
-        elapsed_time = time.time() - start_time
-        remaining_time = duration - elapsed_time
-
-        if remaining_time < 0:
-            lbl_big_time.configure(text="00:00")  # изменяем время на нули по истечению времени
+    while (flag_drop is False) or (flag_stop is False):
+        elapsed_time_1 = time.time() - start_time
+        remaining_time_1 = main_duration - elapsed_time_1
+        if remaining_time_1 < 0:  # не даем уйти счетчику в минус
+            lbl_big_time.configure(text="00:00")
             break
-
-        mins, secs = divmod(remaining_time, 60)  # divmod находит остаток от деления и сам результат деления
+        mins, secs = divmod(remaining_time_1, 60)  # divmod находит остаток от деления и сам результат деления
         time_format = "{:02d}:{:02d}".format(int(mins), int(secs))
         lbl_big_time.configure(text=time_format)
         lbl_big_time.update()  # функция для обновления lbl
         time.sleep(0.1)
 
+
+def timeout(second_duration):  # функция перерыва
+    global flag_stop, flag_drop, time_format
+    flag_drop = False
+    flag_stop = False
+    start_time = time.time()
+    while (flag_drop is False) or (flag_stop is False):
+        elapsed_time_2 = time.time() - start_time
+        remaining_time_2 = second_duration - elapsed_time_2
+        if remaining_time_2 < 0:  # не даем уйти счетчику в минус
+            lbl_small_time.configure(text="00:00")
+            break
+        mins, secs = divmod(remaining_time_2, 60)  # divmod находит остаток от деления и сам результат деления
+        time_format = "{:02d}:{:02d}".format(int(mins), int(secs))
+        lbl_small_time.configure(text=time_format)
+        lbl_small_time.update()  # функция для обновления lbl
+        time.sleep(0.1)
+
+
+def btm_drop_command():  # функция смены флага сброса времени
+    global flag_drop
+    flag_drop = True
+
+
+def btm_stop_command():  # функция смены флага остановки времени
+    global flag_stop
+    flag_stop = True
+
+
+def check_value(value):
+    new_lst = []
+    for y in value:
+        if ',' in y:  # небольшая проверка и замена "," на "."
+            y = y.replace(',', '.')
+            new_lst.append(y)
+        if y.isalpha():
+            lbl_info.configure(text='Вы точно числа вводите? :)')
+        else:
+            new_lst.append(y)
+    return new_lst
+
+
+def stop_to_go():  # остановка времени
+    global time_format, lbl_big_time, lbl_small_time
+    lbl_big_time.configure(text=time_format)
+    lbl_small_time.configure(text=time_format)
+
+
+def total_drop():  # остановка времени
+    global lbl_big_time, lbl_small_time, lbl_amount
+    lbl_big_time.configure(text="00:00")
+    lbl_small_time.configure(text="00:00")
+    lbl_amount.configure(text="00:00")
+
+
 def pomodoro_window():  # функция с оформлением окна
+    global lbl_info, lbl_amount, lbl_main_time, lbl_second_time, lbl_big_time, lbl_small_time, root
     root = tk.Tk()
     root.title("Pomodoro")
     root.geometry("800x340+350+250")
@@ -30,42 +119,44 @@ def pomodoro_window():  # функция с оформлением окна
 
     # блок описывающий кнопки, поля ввода и вспомогательный текст для левого фрейма
 
+    lbl_info = tk.Label(master=frm_left, text='Значения в минутах')
+    lbl_info.config(font=("Courier", 18))
+    lbl_info.grid(row=0, column=0)
     lbl_main_time = tk.Label(master=frm_left, text='Длительность основного времени')
-    lbl_main_time.grid(row=0, column=0)
+    lbl_main_time.grid(row=1, column=0)
 
-    ent_main_time = tk.Entry(master=frm_left)
-    ent_main_time.grid(row=1, column=0)
-
-    btn_main_time = tk.Button(master=frm_left, text='Ок', command=lambda: pomodoro_timer(ent_main_time.get(), lbl_big_time))
-    btn_main_time.grid(row=1, column=1)
-
+    ent_main = tk.Entry(master=frm_left)
+    ent_main.grid(row=2, column=0)
     lbl_second_time = tk.Label(master=frm_left, text='Длительность перерыва')
-    lbl_second_time.grid(row=2, column=0)
+    lbl_second_time.grid(row=3, column=0)
+    ent_second = tk.Entry(master=frm_left)
+    ent_second.grid(row=4, column=0)
+    lbl_amount = tk.Label(master=frm_left, text='Количество')
+    lbl_amount.grid(row=5, column=0)
+    ent_amount = tk.Entry(master=frm_left)
+    ent_amount.grid(row=6, column=0)
 
-    ent_second_time = tk.Entry(master=frm_left)
-    ent_second_time.grid(row=3, column=0)
+    btn_start = tk.Button(master=frm_left, text='Старт', command=lambda: pomodoro_timer(ent_main.get(),
+                                                                                        ent_second.get(),
+                                                                                        ent_amount.get()))
+    btn_start.grid(row=7, column=0, sticky='we')
+    btn_stop = tk.Button(master=frm_left, text='Стоп', command=btm_stop_command)
+    btn_stop.grid(row=7, column=1, sticky='we')
+    btn_drop = tk.Button(master=frm_left, text='Сброс', command=btm_drop_command)
+    btn_drop.grid(row=7, column=2, sticky='we')
 
-    btn_second_time = tk.Button(master=frm_left, text='Ок')
-    btn_second_time.grid(row=3, column=1)
-
-    btn_start = tk.Button(master=frm_left, text='Старт')
-    btn_start.grid(row=4, column=0, sticky='we')
-
-    btn_stop = tk.Button(master=frm_left, text='Стоп')
-    btn_stop.grid(row=4, column=1, sticky='we')
-
-    btn_drop = tk.Button(master=frm_left, text='Сброс')
-    btn_drop.grid(row=4, column=2, sticky='we')
-
-    btn_timer = tk.Button(master=frm_left, text='Timer')
-    btn_timer.grid(row=5, column=0, columnspan=3, sticky='we')
+    btn_timer = tk.Button(master=frm_left, text='Timer', command=btm_timer_command)
+    btn_timer.grid(row=8, column=0, columnspan=3, sticky='we')
 
     # блок изображения времени на правом фрейме
 
     lbl_big_time = tk.Label(master=frm_right, text='--:--')
     lbl_small_time = tk.Label(master=frm_right, text='--:--')
-    lbl_big_time.config(font=("Courier", 100))
-    lbl_small_time.config(font=("Courier", 100))
+    lbl_amount = tk.Label(master=frm_right, text='--')
+    lbl_amount.config(font=("Courier", 50))
+    lbl_big_time.config(font=("Courier", 70))
+    lbl_small_time.config(font=("Courier", 70))
+    lbl_amount.pack()
     lbl_big_time.pack()
     lbl_small_time.pack()
 
--- Screen_timer.py
+++ Screen_timer.py
@@ -1,59 +1,77 @@
 import tkinter as tk
 import time
 
-global flagTest, root, lbl_info
+global flag_drop, root_t, lbl_info, flag_stop, time_format, timer_label
 
 
 def btm_pomodoro_command():  # вызываем через функцию, чтобы избежать циклов вызовов
     from Screen_pomodoro import pomodoro_window
-    root.destroy()
+    root_t.destroy()
     pomodoro_window()
 
 
-def end(timer_label):
+def end():
+    global timer_label
     return timer_label.configure(text="00:00")
 
 
-def timer(value, timer_label):  # основная функция
-    global flagTest, lbl_info
-    flagTest = False
+def timer(value):  # основная функция
+    global flag_drop, lbl_info, flag_stop, time_format, timer_label
+    flag_drop = False
+    flag_stop = False
     if ',' in value:  # небольшая проверка и замена "," на "."
         value = value.replace(',', '.')
     for i in value:
         if i.isalpha():
             lbl_info.configure(text='Нужно число')
-
     duration = float(value) * 60  # сохраняем начальное значение времени
     start_time = time.time()
-    while flagTest is False:
+    while (flag_drop is False) or (flag_stop is False):
         elapsed_time = time.time() - start_time
         remaining_time = duration - elapsed_time
         if remaining_time < 0:                  # не даем уйти счетчику в минус
             timer_label.configure(text="00:00")
             break
-        if flagTest:  # проверка для сброса времени кнопка drop
+        if flag_drop:  # проверка для сброса времени кнопка drop
             break
+        if flag_stop:
+            return stop_to_go()
         mins, secs = divmod(remaining_time, 60)  # divmod находит остаток от деления и сам результат деления
         time_format = "{:02d}:{:02d}".format(int(mins), int(secs))
         timer_label.configure(text=time_format)
         timer_label.update()  # функция для обновления lbl
         time.sleep(0.1)
-    end(timer_label)  # по нажатию drop вызываем функцию завершения
+    end()  # по нажатию drop вызываем функцию завершения
 
 
-def btm_drop_command():  # функция сброса времени
-    global flagTest
-    flagTest = True
+def stop_to_go():  # остановка времени
+    global time_format, timer_label
+    timer_label.configure(text=time_format)
+    # mins, secs = time_format.split(':') подумать, как сделать перезапуск с последним значением
+    # mins = int(mins)
+    # secs = int(secs)
+    # value = mins + (secs / 60)
+    # btn_start = tk.Button(command=lambda: timer(value, timer_label))
+
+
+def btm_drop_command():  # функция смены флага сброса времени
+    global flag_drop
+    flag_drop = True
+
+
+def btm_stop_command():  # функция смены флага остановки времени
+    global flag_stop
+    flag_stop = True
 
 
 def timer_window():  # функция с оформлением окна
-    global root, lbl_info
-    root = tk.Tk()
-    root.title("Timer")
-    root.geometry("800x340+350+250")
-    root.resizable(False, False)
+    global root_t, lbl_info, timer_label
+    root_t = tk.Tk()
+    root_t.title("Timer")
+    root_t.geometry("800x340+350+250")
+    root_t.resizable(False, False)
 
-    frm_left = tk.Frame(master=root)  # фрейм для приема информации
+    frm_left = tk.Frame(master=root_t)  # фрейм для приема информации
     lbl_info = tk.Label(master=frm_left, text='количество минут')
     lbl_info.config(font=("Courier", 13))
     lbl_info.grid(row=0, column=0)
@@ -62,23 +80,31 @@ def timer_window():  # функция с оформлением окна
     btn_start = tk.Button(master=frm_left,  # погнали!
                           text='Start',
                           height=2,
-                          width=18,
-                          command=lambda: timer(ent_time.get(), timer_label))
+                          width=10,
+                          command=lambda: timer(ent_time.get()))
     # подаем в функцию таймера значение времени и "изображение" с lbl
     btn_start.grid(row=2, column=0, sticky='we')
+    btn_stop = tk.Button(master=frm_left,  # погнали!
+                         text='Stop',
+                         height=2,
+                         width=10,
+                         command=btm_stop_command)
+    btn_stop.grid(row=2, column=1, sticky='we')
     btn_drop = tk.Button(master=frm_left,
                          text='Drop',
                          height=2,
-                         width=18, command=btm_drop_command)  # кнопка сброса
-    btn_drop.grid(row=2, column=1)
+                         width=10, command=btm_drop_command)  # кнопка сброса
+    btn_drop.grid(row=2, column=2)
 
     btn_pomodoro_again = tk.Button(master=frm_left, text='Pomodoro', command=btm_pomodoro_command)
     btn_pomodoro_again.grid(row=5, column=0, columnspan=3, sticky='we')
-
-    frm_right = tk.Frame(master=root)  # фрейм для вывода времени
+    root_t.grid_columnconfigure(0, minsize=10)
+    root_t.grid_columnconfigure(1, minsize=10)
+    root_t.grid_columnconfigure(2, minsize=10)
+    frm_right = tk.Frame(master=root_t)  # фрейм для вывода времени
     timer_label = tk.Label(master=frm_right, text="--:--")
     timer_label.config(font=("Courier", 100))
     timer_label.pack()
     frm_left.pack(side=tk.LEFT)
     frm_right.pack(side=tk.RIGHT)
-    root.mainloop()
+    root_t.mainloop()
