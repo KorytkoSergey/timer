a739a09a47a14974f666589dd1b7447522b74fe2
v.savelev <v.saveliev105@gmail.com>
2023-04-08 14:59:43 +0300

    refactoring

--- Screen_1.py
+++ Screen_1.py
@@ -1,39 +1,89 @@
 import tkinter as tk
+from Screen_timer import ScreenTimer
+from Screen_pomodoro import pomodoro_window
 
 
-def btm_timer_command():  # вызываем через функцию, чтобы избежать зациклиности вызовов
-    from Screen_timer import timer_window
-    window_1.destroy()
-    timer_window()
+class FormWorker:
+    root_t, lbl_info, timer_label = None
+    window_1 = tk.Tk()
+    screen_timer = ScreenTimer()
 
 
-def btm_pomodoro_command():  # вызываем через функцию, чтобы избежать циклов вызовов
-    from Screen_pomodoro import pomodoro_window
-    window_1.destroy()
-    pomodoro_window()
+    def draw_main_from(self):
+        self.window_1.title("Timer or Pomodoro")
+        self.window_1.geometry("400x140+550+350")
+        self.window_1.resizable(False, False)
+
+        frm_btn = tk.Frame(master=self.window_1, padx=0, pady=34)  # фрейм кнопок
+
+        btn_timer = tk.Button(master=frm_btn,
+                              text='Timer',
+                              height=2,
+                              width=18,
+                              command=self.btm_timer_command)  # кнопка таймер
+        btn_timer.pack(side=tk.LEFT, fill=tk.BOTH)
+        btn_pomodoro = tk.Button(master=frm_btn,
+                                 text='Pomodoro',
+                                 height=2,
+                                 width=18,
+                                 command=self.btm_pomodoro_command)  # кнопка pomodoro
+        btn_pomodoro.pack(side=tk.RIGHT, fill=tk.BOTH)
+
+        frm_btn.pack(ipadx=12, ipady=12)
+
+        self.window_1.mainloop()
+    def btm_timer_command(self):  # вызываем через функцию, чтобы избежать зациклиности вызовов
+        self.window_1.destroy()
+        self.timer_window()
+
+    def btm_pomodoro_command(self):  # вызываем через функцию, чтобы избежать циклов вызовов
+        self.window_1.destroy()
+        pomodoro_window()
+
+    def timer_window(self):  # функция с оформлением окна
+
+        timer = tk.Tk()
+        timer.title("Timer")
+        timer.geometry("800x340+350+250")
+        timer.resizable(False, False)
+
+        frm_left = tk.Frame(master=timer)  # фрейм для приема информации
+        lbl_info = tk.Label(master=frm_left, text='количество минут')
+        lbl_info.config(font=("Courier", 13))
+        lbl_info.grid(row=0, column=0)
+        ent_time = tk.Entry(master=frm_left)
+        ent_time.grid(row=1, column=0, columnspan=3, sticky='we')
+        btn_start = tk.Button(master=frm_left,  # погнали!
+                              text='Start',
+                              height=2,
+                              width=10,
+                              command=lambda: self.screen_timer.timer(ent_time.get()))
+        # подаем в функцию таймера значение времени и "изображение" с lbl
+        btn_start.grid(row=2, column=0, sticky='we')
+        btn_stop = tk.Button(master=frm_left,  # погнали!
+                             text='Stop',
+                             height=2,
+                             width=10,
+                             command=self.screen_timer.btm_stop_command())
+        btn_stop.grid(row=2, column=1, sticky='we')
+        btn_drop = tk.Button(master=frm_left,
+                             text='Drop',
+                             height=2,
+                             width=10, command=self.screen_timer.btm_stop_command())  # кнопка сброса
+        btn_drop.grid(row=2, column=2)
+
+        btn_pomodoro_again = tk.Button(master=frm_left, text='Pomodoro', command=self.screen_timer.btm_stop_command())
+        btn_pomodoro_again.grid(row=5, column=0, columnspan=3, sticky='we')
+        timer.grid_columnconfigure(0, minsize=10)
+        timer.grid_columnconfigure(1, minsize=10)
+        timer.grid_columnconfigure(2, minsize=10)
+        frm_right = tk.Frame(master=timer)  # фрейм для вывода времени
+        timer_label = tk.Label(master=frm_right, text="--:--")
+        timer_label.config(font=("Courier", 100))
+        timer_label.pack()
+        frm_left.pack(side=tk.LEFT)
+        frm_right.pack(side=tk.RIGHT)
+        timer.mainloop()
 
 
-window_1 = tk.Tk()
-window_1.title("Timer or Pomodoro")
-window_1.geometry("400x140+550+350")
-window_1.resizable(False, False)
 
-frm_btn = tk.Frame(master=window_1, padx=0, pady=34)  # фрейм кнопок
-
-btn_timer = tk.Button(master=frm_btn,
-                      text='Timer',
-                      height=2,
-                      width=18,
-                      command=btm_timer_command)  # кнопка таймер
-btn_timer.pack(side=tk.LEFT, fill=tk.BOTH)
-btn_pomodoro = tk.Button(master=frm_btn,
-                         text='Pomodoro',
-                         height=2,
-                         width=18,
-                         command=btm_pomodoro_command)  # кнопка pomodoro
-btn_pomodoro.pack(side=tk.RIGHT, fill=tk.BOTH)
-
-frm_btn.pack(ipadx=12, ipady=12)
-
-
-window_1.mainloop()
--- Screen_timer.py
+++ Screen_timer.py
@@ -1,110 +1,66 @@
 import tkinter as tk
 import time
 
-global flag_drop, root_t, lbl_info, flag_stop, time_format, timer_label
 
+class ScreenTimer:
+    flag_drop, root_t, lbl_info, flag_stop, time_format, timer_label
 
-def btm_pomodoro_command():  # вызываем через функцию, чтобы избежать циклов вызовов
-    from Screen_pomodoro import pomodoro_window
-    root_t.destroy()
-    pomodoro_window()
+    __var = None
+    def __int__(self, var1):
+        self.var1 = var1
 
+    def btm_pomodoro_command(self):  # вызываем через функцию, чтобы избежать циклов вызовов
+        from Screen_pomodoro import pomodoro_window
+        root_t.destroy()
+        pomodoro_window()
 
-def end():
-    global timer_label
-    return timer_label.configure(text="00:00")
+    def end(self):
+        global timer_label
+        return timer_label.configure(text="00:00")
 
+    def timer(self, value):  # основная функция
+        global flag_drop, lbl_info, flag_stop, time_format, timer_label
+        flag_drop = False
+        flag_stop = False
+        if ',' in value:  # небольшая проверка и замена "," на "."
+            value = value.replace(',', '.')
+        for i in value:
+            if i.isalpha():
+                lbl_info.configure(text='Нужно число')
+        duration = float(value) * 60  # сохраняем начальное значение времени
+        start_time = time.time()
+        while (flag_drop is False) or (flag_stop is False):
+            elapsed_time = time.time() - start_time
+            remaining_time = duration - elapsed_time
+            if remaining_time < 0:  # не даем уйти счетчику в минус
+                timer_label.configure(text="00:00")
+                break
+            if flag_drop:  # проверка для сброса времени кнопка drop
+                break
+            if flag_stop:
+                return stop_to_go()
+            mins, secs = divmod(remaining_time, 60)  # divmod находит остаток от деления и сам результат деления
+            time_format = "{:02d}:{:02d}".format(int(mins), int(secs))
+            timer_label.configure(text=time_format)
+            timer_label.update()  # функция для обновления lbl
+            time.sleep(0.1)
+        end()  # по нажатию drop вызываем функцию завершения
 
-def timer(value):  # основная функция
-    global flag_drop, lbl_info, flag_stop, time_format, timer_label
-    flag_drop = False
-    flag_stop = False
-    if ',' in value:  # небольшая проверка и замена "," на "."
-        value = value.replace(',', '.')
-    for i in value:
-        if i.isalpha():
-            lbl_info.configure(text='Нужно число')
-    duration = float(value) * 60  # сохраняем начальное значение времени
-    start_time = time.time()
-    while (flag_drop is False) or (flag_stop is False):
-        elapsed_time = time.time() - start_time
-        remaining_time = duration - elapsed_time
-        if remaining_time < 0:                  # не даем уйти счетчику в минус
-            timer_label.configure(text="00:00")
-            break
-        if flag_drop:  # проверка для сброса времени кнопка drop
-            break
-        if flag_stop:
-            return stop_to_go()
-        mins, secs = divmod(remaining_time, 60)  # divmod находит остаток от деления и сам результат деления
-        time_format = "{:02d}:{:02d}".format(int(mins), int(secs))
+    def stop_to_go(self):  # остановка времени
+        global time_format, timer_label
         timer_label.configure(text=time_format)
-        timer_label.update()  # функция для обновления lbl
-        time.sleep(0.1)
-    end()  # по нажатию drop вызываем функцию завершения
+        # mins, secs = time_format.split(':') подумать, как сделать перезапуск с последним значением
+        # mins = int(mins)
+        # secs = int(secs)
+        # value = mins + (secs / 60)
+        # btn_start = tk.Button(command=lambda: timer(value, timer_label))
+
+    def btm_drop_command(self):  # функция смены флага сброса времени
+        global flag_drop
+        flag_drop = True
+
+    def btm_stop_command(self):  # функция смены флага остановки времени
+        global flag_stop
+        flag_stop = True
 
 
-def stop_to_go():  # остановка времени
-    global time_format, timer_label
-    timer_label.configure(text=time_format)
-    # mins, secs = time_format.split(':') подумать, как сделать перезапуск с последним значением
-    # mins = int(mins)
-    # secs = int(secs)
-    # value = mins + (secs / 60)
-    # btn_start = tk.Button(command=lambda: timer(value, timer_label))
-
-
-def btm_drop_command():  # функция смены флага сброса времени
-    global flag_drop
-    flag_drop = True
-
-
-def btm_stop_command():  # функция смены флага остановки времени
-    global flag_stop
-    flag_stop = True
-
-
-def timer_window():  # функция с оформлением окна
-    global root_t, lbl_info, timer_label
-    root_t = tk.Tk()
-    root_t.title("Timer")
-    root_t.geometry("800x340+350+250")
-    root_t.resizable(False, False)
-
-    frm_left = tk.Frame(master=root_t)  # фрейм для приема информации
-    lbl_info = tk.Label(master=frm_left, text='количество минут')
-    lbl_info.config(font=("Courier", 13))
-    lbl_info.grid(row=0, column=0)
-    ent_time = tk.Entry(master=frm_left)
-    ent_time.grid(row=1, column=0, columnspan=3, sticky='we')
-    btn_start = tk.Button(master=frm_left,  # погнали!
-                          text='Start',
-                          height=2,
-                          width=10,
-                          command=lambda: timer(ent_time.get()))
-    # подаем в функцию таймера значение времени и "изображение" с lbl
-    btn_start.grid(row=2, column=0, sticky='we')
-    btn_stop = tk.Button(master=frm_left,  # погнали!
-                         text='Stop',
-                         height=2,
-                         width=10,
-                         command=btm_stop_command)
-    btn_stop.grid(row=2, column=1, sticky='we')
-    btn_drop = tk.Button(master=frm_left,
-                         text='Drop',
-                         height=2,
-                         width=10, command=btm_drop_command)  # кнопка сброса
-    btn_drop.grid(row=2, column=2)
-
-    btn_pomodoro_again = tk.Button(master=frm_left, text='Pomodoro', command=btm_pomodoro_command)
-    btn_pomodoro_again.grid(row=5, column=0, columnspan=3, sticky='we')
-    root_t.grid_columnconfigure(0, minsize=10)
-    root_t.grid_columnconfigure(1, minsize=10)
-    root_t.grid_columnconfigure(2, minsize=10)
-    frm_right = tk.Frame(master=root_t)  # фрейм для вывода времени
-    timer_label = tk.Label(master=frm_right, text="--:--")
-    timer_label.config(font=("Courier", 100))
-    timer_label.pack()
-    frm_left.pack(side=tk.LEFT)
-    frm_right.pack(side=tk.RIGHT)
-    root_t.mainloop()
